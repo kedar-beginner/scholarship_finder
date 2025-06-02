0ce2993a455802a27ecf71fb05093f65737ce9ba
Aniket-ing <ingleniket211@gmail.com>
2025-06-03 00:32:27 +0530

    update for deploy

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,11 @@ import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/comapny.route.js"
 import scholarshipRoute from "./routes/scholarship.route.js"
 import applicationRoute  from "./routes/application.route.js";
+import path from "path";
 dotenv.config({});
 const app=express();
 
+const _dirname= path.resolve();
   
 //middleware
 app.use(express.json());
@@ -28,6 +30,11 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/scholarship",scholarshipRoute);
 app.use("/api/v1/application",applicationRoute);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 
 app.listen(PORT,()=>{
     connectDB();
